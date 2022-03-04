ebcc96a71100d3df2ffbefd9c2d2486af4470387
bboygg <bboygg94@gmail.com>
2022-03-04 21:54:41 +0900

    implemented to p.122 Will resume from Action 10: 근데... 이거 누가 쓴 트윗이야?

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
 import { dbService } from "fbase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id};
+            setNweets((prev) => [nweetObject, ...prev])
+        });
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -22,6 +36,7 @@ const Home = () => {
     };
 
     return (
+        <>
         <form onSubmit={onSubmit}>
             <input
             value={nweet}
@@ -30,8 +45,19 @@ const Home = () => {
             placeholder="What's on your mind?"
             maxLength={120}
             />
-            <input type="submit" value="Nweet" />
+            <input 
+            type="submit" 
+            value="Nweet" 
+            />
         </form>
+        <div>
+            {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.text}</h4>
+                </div>
+            ))}
+        </div>
+        </>
     );
 };
 
