a24ce03c3868e786f0f98410c57872380faca6e0
bboygg <bboygg94@gmail.com>
2022-03-06 18:39:19 +0900

    implement delete tweet function

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,10 +1,21 @@
+import { dbService } from "fbase";
+
 const Nweet = ({ nweetObj, isOwner }) => {
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete tweet?");
+        console.log(ok);
+        if (ok) {
+            console.log(nweetObj.id);
+            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            console.log(data);
+        }
+    }
     return (
         <div>
             <h4>{nweetObj.text}</h4>
             {isOwner && (
                 <>
-                    <button>Delete Nweet</button>
+                    <button onClick={onDeleteClick}>Delete Nweet</button>
                     <button>Edit Nweet</button>
                 </>
             )}
