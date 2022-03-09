1144d1e72e51f4f68556462a3173d043fce6a9fc
bboygg <bboygg94@gmail.com>
2022-03-09 22:57:52 +0900

    test

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -46,9 +46,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
                 <>
                 <h4>{nweetObj.text}</h4>
                 {nweetObj.attachmentUrl && (
-                    <img src={nweetObj.attachmentUrl} width="50px" height="50px" />
+                    <img src={nweetObj.attachmentUrl} width="100px" height="100px" />
                 )}
-                    {isOwner && (
+                    {isOwner &&(
                     <>
                         <button onClick={onDeleteClick}>Delete Nweet</button>
                         <button onClick={toggleEditing}>Edit Nweet</button>
